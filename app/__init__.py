bd9fb6aa0008979b42ad7f1d1b9212f45bd2a436
chepmanu <emmanuelchepsire33@gmail.com>
2018-04-26 11:36:26 +0300

    Used OOP structure for the data2.py file for the endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
+'''
 from flask import Flask, request, jsonify
-app = Flask(__name__)
+app2 = Flask(__name__)
 import json
 
 menu= []
@@ -11,7 +12,7 @@ orders = [{"food":"beeans and chapati", "id":4, "price":200}]
 meals = [{"food":"pilau and beef", "id":3, "price":350}]
 
 #login endpoint
-@app.route('/login')
+@app2.route('/login')
 def login():
     user = request.get_json()
     username = user['username']
@@ -25,7 +26,7 @@ def login():
     #still woiking on it
 
 #registration endpoin
-@app.route('/register', methods=['POST'])
+@app2.route('/register', methods=['POST'])
 def register():
     new_user = request.get_json()
     username = new_user['username']
@@ -44,13 +45,13 @@ def register():
     
     
 #api endpoint for viewing the set menu.
-@app.route('/menu/', methods=['GET'])
+@app2.route('/menu/', methods=['GET'])
 def getmenu_endpoint():
     data = jsonify(menu)
     return data
 
 #api endpoint for making an order basing on the menu
-@app.route('/makeorder', methods=['GET', 'POST'])
+@app2.route('/makeorder', methods=['GET', 'POST'])
 def selectmealfrommenu_endpoint():
     order = request.get_json()
     if menu is None:
@@ -92,7 +93,7 @@ def deletemeal_endpoint(id):
     for item in menu:
         if item['id'] == id:
             menu.remove(item)
-            print(menu)
+    print(menu)
     return json.dumps(menu)
 
 #getting one meal endpoint
@@ -124,3 +125,139 @@ def getoneoder_endpoint(id):
     
 if __name__ == "__main__":
     app.run(port=6500)
+'''
+'''
+from flask import Flask, request, jsonify
+import json
+import uuid
+import os
+#import jwt
+from functools import wraps
+import datetime
+from werkzeug.security  import generate_password_hash, check_password_hash
+app = Flask(__name__)
+
+
+class User():
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        orders = []
+      
+    #def add_meal(self, meal):
+     #   self.meals.append(meal)
+
+    def getalldetails(self):
+        return (self.username, self.password, self.email)
+      
+      
+class Order():
+    def __init__(self, food, price):
+        self.food = food
+        self.price = price
+      
+class Meal():
+    def __init__(self, food, price, id):
+        self.food = food
+        self.price = price
+        self.id = id
+
+    
+    def __iter__(self):
+        return self.__dict__.iteritems()
+        
+
+
+users = []
+meals = []
+### To add new user during signup
+@app.route('/signup', methods=['POST'])
+def register_endpoint():
+    username = request.get_json().get('username', None)
+    password = request.get_json().get('password')
+    email = request.get_json().get('email', None)
+
+    user = User(username=username, password=password, email=email)
+    users.append(user)
+    print(users)
+    return jsonify({"message":"Signed up"})
+
+##### During log in
+
+# Get username and password from request then
+@app.route('/login', methods=['POST'])
+def login():
+    for user in users:
+        username = request.get_json().get('username')
+        password = request.get_json().get('password')
+        if user.username == username:
+            if user.password == password:
+                return "login success"
+            else:
+                return "Invalid password"
+      
+    return 'Invalid username'
+      
+####### TO add a meal for a given user
+@app.route('/add_meal', methods=['POST'])
+def add_meal():
+    food = request.get_json().get('food')
+    price = request.get_json().get('price')
+    id = request.get_json().get('id')
+    #get user from list then 
+
+    meal = Meal(food=food, price=price, id=id)
+    meals.append(meal)
+    #user.add_meal(meal)
+    print(meal.food)
+    print(dict(meal))
+    return jsonify({"message":"meal added"})
+
+#To get meal option by admin
+@app.route('/get_meals', methods=['GET'])
+def get_meals():
+    for i in meals:
+        print(i.food)
+    return jsonify({"message":"This are the items"})
+
+#update a meal option by admin
+@app.route('/mordify_meal/<id>', methods=['PUT'])
+def mordify_meal(id):
+    new_food = request.get_json().get('food')
+    new_price = request.get_json().get('price')
+    new_id = request.get_json().get('id')
+    new_meal = Meal(food=new_food, price=new_price, id=new_id)
+    prev_food = []
+    #for i in meals:
+   #     if i.id == id:
+    #        meals.remove(i)
+    for i in meals:
+        if i.id == id:
+            prev_food.append(i.food)
+    print(prev_food)
+    print(new_meal.food)
+    #if order not in orders:
+    #    return jsonify({"message":"order not found"})
+    
+    for i,item in enumerate(meals):
+        if item == prev_food[0]:
+            meals[i]=new_meal
+    for i in meals:
+        print(i.food)
+    return jsonify({"message":"Done"})
+
+#Remove  A meal option
+@app.route('/delete/<id>', methods=['DELETE'])
+def delete(id):
+    for i in meals:
+        if i.id == id:
+            meals.remove(i)
+    for i in meals:
+        print(i.food)
+    return jsonify({"message":"Item removed"})
+
+
+if __name__ == '__main__':
+    app.run(port=7300, debug=True)
+'''
---
+++ app/data.py
@@ -0,0 +1,32 @@
+menu = [{"food":"rice and beef", "id":1, "price":250},
+        {"food":"bread and tea", "id":2, "price":50},
+        {"food":"pilau and beef", "id":3, "price":350},
+        {"food":"beeans and chapati", "id":4, "price":200} ]
+
+users = [{"username": "Tom", "password":"tomylin"},
+        {"username": "Angie", "password":"angelican"}]
+
+'''
+class Users:
+    def __init__(self, username, password, id):
+        self.username = username
+        self.password = password
+        self.id = id
+
+class Users(customer):
+    def __init__(self. username, password, id, admin)
+        User.__init__(admin)
+        self.admin = False
+
+class Users(caterer):
+    def __init__(self, username, password, id, admin)
+        User.__init__(admin)
+        self.admin = admin
+
+class Meals:
+    def __init__(self, food, price, id):
+        self.food = food
+        self.price = price 
+        self.id = id
+'''
+
